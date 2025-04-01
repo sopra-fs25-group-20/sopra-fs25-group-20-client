558e8270cd90010a9154441ab64c3248195d33f1
baranozgurtas <baranoezguer.tas@uzh.ch>
2025-04-01 15:30:08 +0200

    feat: create start and join game UI on main page

--- app/components/PrimaryButton.tsx
+++ app/components/PrimaryButton.tsx
@@ -1,32 +1,25 @@
 "use client";
+
 interface PrimaryButtonProps {
-    label: string;
-    onClick?: () => void;
-    type?: "button" | "submit";
-    className?: string;
-  }
-  
-  export const PrimaryButton = ({
-    label,
-    onClick,
-    type = "button",
-    className = "",
-  }: PrimaryButtonProps) => {
-    return (
-      <button
-        type={type}
-        onClick={onClick}
-        className={`btn btn-primary fw-bold ${className}`}
-        style={{
-          backgroundColor: "var(--user-color-1)",
-          color: "#fff",
-          border: "none",
-          borderRadius: "8px",
-          padding: "8px 12px",
-        }}
-      >
-        {label}
-      </button>
-    );
-  };
-  
+  label: string;
+  onClick?: () => void;
+  type?: "button" | "submit";
+  className?: string;
+}
+
+export const PrimaryButton = ({
+  label,
+  onClick,
+  type = "button",
+  className = "",
+}: PrimaryButtonProps) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`btn ${className}`}
+    >
+      {label}
+    </button>
+  );
+};
