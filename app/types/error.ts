1f25120abbf9b88464d9bcfb64d7c713b3208fec
Dreamfarer <dreamfarer@pm.me>
2025-04-02 16:29:19 +0200

    refactor: use 'apiService' instead of raw fetch

--- app/types/error.ts
+++ app/types/error.ts
@@ -1,4 +1,12 @@
-export interface ApplicationError extends Error {
-  info: string;
+export class ApplicationError extends Error {
   status: number;
+  info: string;
+
+  constructor(status: number, info: string) {
+    super(info);
+    this.name = "ApplicationError";
+    this.status = status;
+    this.info = info;
+    Object.setPrototypeOf(this, ApplicationError.prototype);
+  }
 }
