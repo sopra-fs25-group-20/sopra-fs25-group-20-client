2802044b02f62188f6308e1b5c77ab43a67f1897
dreamfarer <dreamfarer@pm.me>
2025-04-13 21:54:17 +0200

    feat(VerticalFlex): add option to force specific width

--- app/components/verticalFlex.tsx
+++ app/components/verticalFlex.tsx
@@ -3,10 +3,22 @@
 interface VerticalFlexProps {
   children: React.ReactNode;
   gap?: number;
+  width?: number;
 }
 
-export const VerticalFlex = ({ children, gap = 30 }: VerticalFlexProps) => (
-  <div className="vertical-flex" style={{ gap: `${gap}px` }}>
+export const VerticalFlex = ({ children, gap = 30, width }: VerticalFlexProps) => (
+  <div
+    className="vertical-flex"
+    style={{
+      gap: `${gap}px`,
+      ...(width !== undefined
+        ? {
+            width: `${width}px`,
+            flex: "unset",
+          }
+        : {}),
+    }}
+  >
     {children}
   </div>
 );
