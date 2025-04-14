61092dbbb9b55257d03ee5c7abe9eacfebc3102d
Dreamfarer <dreamfarer@pm.me>
2025-04-14 17:26:08 +0200

    feat(Gallery): highlight correct image if innocent

--- app/components/gallery.tsx
+++ app/components/gallery.tsx
@@ -4,45 +4,93 @@ import { useApi } from "@/hooks/useApi";
 import { Frame } from "./frame";
 import { useEffect, useState } from "react";
 import { stompApi } from "@/api/stompApi";
+import { Role } from "@/types/role";
+import { useGame } from "@/hooks/useGame";
+import { HighlightedImage } from "@/types/highlightedImage";
 
 export const Gallery = () => {
   const apiService = useApi();
+  const gameApi = useGame();
   const roomCode = stompApi.getCode();
+  const [role, setRole] = useState<string | null>(null);
+  const [highlightedImage, setHighlightedImage] = useState<number | null>(null);
   const [imageList, setImageList] = useState<(string | null)[]>(Array(9).fill(null));
 
-  useEffect(() => {
-    const fetchImages = async () => {
-      const newImageList: (string | null)[] = [...imageList];
+  /**
+   * Handles receptions of changed game role.
+   */
+  const handleRole = (data: Role) => {
+    setRole(data.playerRole);
+  };
 
-      await Promise.all(
-        Array.from({ length: 9 }, async (_, i) => {
-          try {
-            const blob = await apiService.get<Blob>(`/image/${roomCode}/${i}`, false);
-            newImageList[i] = URL.createObjectURL(blob);
-          } catch (err) {
-            console.error(`Failed to load image at index ${i}`, err);
-            newImageList[i] = null;
-          }
-        })
-      );
+  /**
+   * Request broadcasting of role.
+   */
+  const requestRole = async () => {
+    gameApi.requestRole();
+  };
 
-      setImageList(newImageList);
-    };
-
-    if (roomCode) {
-      fetchImages();
+  /**
+   * Handles reception of the highlighted image
+   */
+  const handleHighlightedImage = (data: HighlightedImage) => {
+    console.warn(data.index)
+    if (data.index >= 0) {
+      setHighlightedImage(data.index);
     }
-  }, [apiService, roomCode]);
+  };
+
+  /**
+   * Request broadcasting of the highlighted image.
+   */
+  const requestHighlightedImage = async () => {
+    gameApi.requestHighlightedImage();
+  };
+
+  /**
+   * Fetch all 9 images and store them in a list.
+   */
+  const fetchImages = async () => {
+    const newImageList: (string | null)[] = [...imageList];
+
+    await Promise.all(
+      Array.from({ length: 9 }, async (_, i) => {
+        try {
+          const blob = await apiService.get<Blob>(`/image/${roomCode}/${i}`, false);
+          newImageList[i] = URL.createObjectURL(blob);
+        } catch (err) {
+          console.error(`Failed to load image at index ${i}`, err);
+          newImageList[i] = null;
+        }
+      })
+    );
+
+    setImageList(newImageList);
+  };
 
   const handleImageClick = (index: number) => {
     console.log("Image clicked:", index);
   };
 
+  /**
+   * Register handlers in game api and fetch images.
+   */
+  useEffect(() => {
+    gameApi.onRole(handleRole);
+    gameApi.onHighlightedImage(handleHighlightedImage)
+    requestRole();
+    requestHighlightedImage()
+    fetchImages();
+  }, [gameApi]);
+
   return (
     <Frame className="gallery">
       <div className="image-grid ">
         {imageList.map((url, index) => (
-          <div key={index} className="image-container">
+          <div
+            key={index}
+            className={`image-container ${role === "innocent" && highlightedImage === index ? "highlight" : ""}`}
+          >
             {url && (
               <img
                 src={url}
@@ -52,6 +100,7 @@ export const Gallery = () => {
               />
             )}
           </div>
+
         ))}
       </div>
     </Frame>
