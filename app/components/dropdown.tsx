f7ec4b5aab7bffdfd94bd1d3e21ff3ab42305e03
Dreamfarer <dreamfarer@pm.me>
2025-04-17 19:23:25 +0200

    fix(Settings): disable option to change settings for non-admin

--- app/components/dropdown.tsx
+++ app/components/dropdown.tsx
@@ -5,17 +5,19 @@ interface DropdownProps {
   options: string[] | number[];
   value: string | number;
   onChange: (value: string | number) => void;
+  disabled?: boolean;
 }
 
 export const Dropdown = (
-  { label, options, value, onChange }: DropdownProps,
+  { label, options, value, onChange, disabled = false }: DropdownProps,
 ) => (
   <div className="setting">
     <label className="half-button">{label}:</label>
     <select
       value={value}
       onChange={(e) => onChange(e.target.value)}
-      className="dropdown"
+      className={`dropdown ${disabled ? "disabled" : ""}`}
+      disabled={disabled}
     >
       {options.map((option) => (
         <option key={option} value={option}>
