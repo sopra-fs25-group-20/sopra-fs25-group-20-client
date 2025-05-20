2ea9e0c9f868ccb72793d9b4d56f1847fcf2a58e
dreamfarer <dreamfarer@pm.me>
2025-05-20 11:25:30 +0200

    feat(StatisticsCard): add button to play SpyQuest

--- app/components/statisticsCard.tsx
+++ app/components/statisticsCard.tsx
@@ -2,42 +2,54 @@
 
 import { Frame } from "./frame";
 import { Account } from "@/types/account";
+import { HorizontalFlex } from "./horizontalFlex";
+import { Button } from "./Button";
+import { useRouter } from "next/navigation";
+import { VerticalFlex } from "./verticalFlex";
 
 type Props = {
   profile: Account;
 };
 
 export const StatisticsCard = ({ profile }: Props) => {
+  const router = useRouter();
+
+  const handleButtonCLick = () => {
+    router.push("/gamesetup");
+  };
+
   return (
-    <Frame className="statistics">
-      <div className="statistics entry">
-        <div className="title">Wins</div>
-        <div className="value">{profile.wins}</div>
-      </div>
-      <div className="statistics entry">
-        <div className="title">Defeats</div>
-        <div className="value">{profile.defeats}</div>
-      </div>
-      <div className="statistics entry">
-        <div className="title">Win Rate</div>
-        <div className="value">
-          {profile.wins + profile.defeats === 0
-            ? "N/A"
-            : `${
-              ((profile.wins / (profile.wins + profile.defeats)) * 100).toFixed(
-                2,
-              )
-            }%`}
+    <Frame>
+      <VerticalFlex hug gap={5}>
+        <div className="statistics entry">
+          <div className="title">Wins</div>
+          <div className="value">{profile.wins}</div>
         </div>
-      </div>
-      <div className="statistics entry">
-        <div className="title">Current Streak</div>
-        <div className="value">{profile.current_streak}</div>
-      </div>
-      <div className="statistics entry">
-        <div className="title">Highest Streak</div>
-        <div className="value">{profile.highest_streak}</div>
-      </div>
+        <div className="statistics entry">
+          <div className="title">Defeats</div>
+          <div className="value">{profile.defeats}</div>
+        </div>
+        <div className="statistics entry">
+          <div className="title">Win Rate</div>
+          <div className="value">
+            {profile.wins + profile.defeats === 0
+              ? "N/A"
+              : `${(
+                  (profile.wins / (profile.wins + profile.defeats)) *
+                  100
+                ).toFixed(2)}%`}
+          </div>
+        </div>
+        <div className="statistics entry">
+          <div className="title">Current Streak</div>
+          <div className="value">{profile.current_streak}</div>
+        </div>
+        <div className="statistics entry">
+          <div className="title">Highest Streak</div>
+          <div className="value">{profile.highest_streak}</div>
+        </div>
+      </VerticalFlex>
+      <Button onClick={handleButtonCLick}>Play SpyQuest</Button>
     </Frame>
   );
 };
